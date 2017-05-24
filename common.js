948bdd1966b9f13640c706928021e0b5d9edcc0e
Herr-Gunter <kubyshkin1990@mail.ru>
2017-05-24 16:31:12 +0300

    begining of sily story

--- common.js
+++ common.js
@@ -1,224 +1,45 @@
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
-  
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
+var insertX =['Willy the Goblin',
+              'Big Daddy Father',
+              'Christmas'];
 
-  var guesses = document.querySelector('.guesses');
-  var lastResult = document.querySelector('.lastResult');
-  var lowOrHi = document.querySelector('.lowOrHi');
-  var guessSubmit = document.querySelector('.guessSubmit');
-  var guessField = document.querySelector('.guessField');
-  var list = document.querySelector('.list ');
-  var guessCount = 1;
-  var resetButton;
-  function checkGuess() {
- 	var randomNumber = Math.floor(Math.random()*100) + 1;
-    var userGuess = Number(guessField.value);
-    if(guessCount === 1) {
-      guesses.textContent = 'Previous guesses: ';
-    }
-    guesses.textContent += userGuess + ' ';
- 
-    if(userGuess === randomNumber) {
-      lastResult.textContent = 'Congratulations! You got it right!';
-      lastResult.style.backgroundColor = 'green';
-      lowOrHi.textContent = '';
-      setGameOver();
-    } else if(guessCount === 10) {
-      lastResult.textContent = '!!!GAME OVER!!!';
-      setGameOver();
-    } else {
-      lastResult.textContent = 'Wrong!';
-      lastResult.style.backgroundColor = 'red';
-      if(userGuess < randomNumber) {
-        lowOrHi.textContent = 'Last guess was too low!';
-      } else if(userGuess > randomNumber) {
-        lowOrHi.textContent = 'Last guess was too high!';
-      }
-    }
- 
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
-  }
-  guessSubmit.addEventListener('click', checkGuess);
-  
-  function setGameOver() {
-	  guessField.disabled = true;
-	  guessSubmit.disabled = true;
-	  resetButton = document.createElement('button');
-	  resetButton.textContent = 'Start new game';
-	  document.body.appendChild(resetButton);
-	  resetButton.addEventListener('click', resetGame);
-  }
-  
-  function resetGame() {
-	  guessCount = 1;
-	
-	  var resetParas = document.querySelectorAll('.resultParas p');
-	  for(var i = 0; i < resetParas.length; i++) {
-		  resetParas[i].textContent = '';
-	  }
-	  resetButton.parentNode.removeChild(resetButton);
-	
-	  guessField.disabled = false;
-	  guessSubmit.disabled = false;
-	  guessField.value = '';
-	  guessField.focus();
-	
-	  lastResult.style.backgroundColor = 'white';
-	
-	 // randomNumber = Math.floor(Math.random()) + 1;
-  }
+var insertY = ['the soup kitchen', 
+                'Disneyland',
+                'the White House'];
 
-  function sortList(){
-   var arr = [ 'Happy Birthday!',
-            ' Merry Christmas my love',
-            'A happy Christmas to all the family',
-             'Youre all I want for Christmas',
-              ' Get well soon'
-              ];
-     for(var i = 0; i < arr.length; i++){
-      if(arr[i].indexOf('Christmas') >= 1){
-        var newLi = document.createElement('li');
-        newLi.textContent = arr[i];
-        list.appendChild(newLi);
-      }
-    }
-  }
-
-  sortList();
+var insertZ = ['spontaneously combusted',
+               'melted into a puddle on the sidewalk', 
+               'turned into a slug and crawled away'];
 
 
-  function extractNewData(){
-    var stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
-                'GNF576746573fhdg4737dh4;Greenfield',
-                'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
-                'SYB4f65hf75f736463;Stalybridge',
-                'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'];
-
-    var currentSplited = stations.join(";");
-    var separateArray = currentSplited.split(";");
-    var newArr = [];
-    var secArr =[];
-      for(var i = 0; i < stations.length; i++){  
-        
-        var current = stations[i].slice(0, 3);
-        newArr.push(current);
-    }
-
-      for(var j = 0 ; j < separateArray.length; j++ ){
-       if (j%2 != 0 && j!= 0){
-        secArr.push(separateArray[j]);
-        }
-     }
-   //  alert(secArr);
-     for( var a = 0; a < secArr.length; a ++){
-        var newList = document.createElement('li');
-        newList.textContent += newArr[a] + " : " +  secArr[a] + " ";
-
-        list.appendChild(newList);
-    }
-
-  }
-  extractNewData();
-
-
-function work(){
-
-var list = document.querySelector('.price');
-var totalBox = document.querySelector('.caution');
-var total = 0;
- list.innerHTML = '';
- totalBox.textContent = '';
-//number 1
-var products =  [ 'Underpants:6.99',
-                'Socks:5.99',
-                'T-shirt:14.99',
-                'Trousers:31.99',
-                'Shoes:23.99'];
-var price = [];
-var nameOfproduct = [];
-for (var i = 0; i < products.length; i++) { // number 2
-  // number 3
-  var separProducts = products[i].split(':');
-  price[i] = Number(separProducts[1]);
-  nameOfproduct[i] = separProducts[0];
-  // number 4
-  
-  // number 5
-  var itemText = nameOfproduct[i] + " - $" + price[i];
-  //itemText += price[i];
-  var listItem = document.createElement('li');
-  listItem.textContent = itemText;
-  list.appendChild(listItem);
-  total += price[i];
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
 }
-  
-  alert(typeof(price[0]));
-  totalBox.textContent = 'Total: $' + total.toFixed(2);
 
-}
-work();
+randomize.addEventListener('click', result);
 
-  function chat(){
+function result() {
+  var newStory = storyText;
+  var itemX = randomValueFromArray(insertX);
+  var itemY = randomValueFromArray(insertY);
+  var itemZ = randomValueFromArray(insertZ);
 
-  var list = document.querySelector('.out ul');
-  var searchInput = document.querySelector('.out input');
-  var searchBtn = document.querySelector('.out button');
-
-  list.innerHTML = '';
-
-  var myHistory = [];
-
-  searchBtn.addEventListener('click',start );
-  searchBtn.onkeypress = function(e){
-   // var key = e.keyCode;
-  //  if(key == 13){
-      start();
-    //}
-  }
-
-  function press(e){
-   var key = e.keyCode || e.which;
-    if(key == 13){
-      start();
-    }
-  }
-
-  function start() {
-  // we will only allow a term to be entered if the search input isn't empty
-  if (searchInput.value !== '') {
-    // number 1
-  // empty the list so that we don't display duplicate entries
-    // the display is regenerated every time a search term is entered.
-
-    var current = searchInput.value;
-    list.innerHTML = '';
-    myHistory.unshift(current);
-   
-    // loop through the array, and display all the search terms in the list
-    for (var i = 0; i < myHistory.length; i++) {
-       
-       var itemText = myHistory[i];
-       var listItem = document.createElement('li');
-       listItem.textContent = itemText;
-       list.appendChild(listItem);
+  if(customName.value != '') {
+    var name = customName.value;
     
-    }
-     if (myHistory.length >= 5) {
-      // number 2
-   
-      myHistory.pop();
-    
-      }
-
-    // If the array length is 5 or more, remove the oldest search term
-   
-    // empty the search input and focus it, ready for the next term to be entered
-    searchInput.value = '';
-    searchInput.focus();
-    }
   }
-}
-chat();
 
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300);
+    var temperature =  Math.round(94);
+    
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
